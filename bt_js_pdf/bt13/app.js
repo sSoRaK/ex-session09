a21db8c45f05b992d12a708f4583760f110f178f
sSoRaK <thong9515@gmail.com>
2023-06-02 16:57:29 +0900

    bo sung bai 13

--- bt_js_pdf/bt13/app.js
+++ bt_js_pdf/bt13/app.js
@@ -1,1 +1,9 @@
 // Nhập vào 1 số, in ra số dư trong phép chia 3 (Sử dụng toán tử 3 ngôi lồng nhau)
+// remainder : số dư
+const number = parseFloat(prompt("Nhập 1 số nguyên"));
+const remainder = number % 3;
+remainder === 0
+  ? document.write("số dư là 0")
+  : remainder === 1
+  ? document.write("số dư là 1")
+  : document.write("số dư là 2");
